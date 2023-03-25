aece5097e1c36c6088fea08278eda67e423ed205
Fernando Costa <nandoantoniocosta@gmail.com>
2023-03-25 14:34:57 -0300

    cleanup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: true,
   sourcemap: true,
   clean: true,
   platform: 'neutral',
